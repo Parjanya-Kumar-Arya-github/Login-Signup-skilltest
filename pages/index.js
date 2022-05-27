58260236b2995d0e185c4357a46b32b625951eda
Parjanya Kumar Arya <Parjanya789@gmail.com>
2022-05-27 19:08:35 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Link from "next/Link";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 export default function Home() {
